221c4fdb66c8b38a47879ba977cb212bf57198fa
yz19a <yanelperaltarodriguez77@gmail.com>
2025-03-19 14:25:02 +0100

    updated include folder

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: yaperalt <yaperalt@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 00:17:20 by yaperalt          #+#    #+#             */
-/*   Updated: 2025/03/07 19:28:03 by yaperalt         ###   ########.fr       */
+/*   Updated: 2025/03/16 10:36:49 by yaperalt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,14 @@
 
 typedef struct s_stack
 {
+	int		value;
+	int		fix_v;
+	s_stack	*prev;
 	s_stack	*next;
 }	t_stack;
 
+// utils.c
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
 
 #endif
