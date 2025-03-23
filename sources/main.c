2011f7f788920d480c0dc141bccdd31d46ebae86
yz19a <yanelperaltarodriguez77@gmail.com>
2025-03-23 16:00:08 +0100

    updated main.c

--- sources/main.c
+++ sources/main.c
@@ -6,13 +6,36 @@
 /*   By: yaperalt <yaperalt@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 13:47:56 by yaperalt          #+#    #+#             */
-/*   Updated: 2025/03/15 22:51:50 by yaperalt         ###   ########.fr       */
+/*   Updated: 2025/03/19 15:18:57 by yaperalt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
+#include <stdio.h>
+
+void	print_list(t_stack *stack)
+{
+	while (stack)
+	{
+		printf("%d\n", stack->value);
+		stack = stack->next;
+	}
+}
 
 int	main(int argc, char **argv)
 {
+	t_stack	*a;
+	t_stack	*b;
 
+	a = NULL;
+	b = NULL;
+	if (argc < 2)
+	{
+		correct_usage();
+		return (1);
+	}
+	parse_args(&a, argc, argv);
+	print_list(a);
+	free_list(a);
+	return (EXIT_SUCCESS);
 }
