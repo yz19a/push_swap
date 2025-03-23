240dd3c28ef461fc2cbe51bba1c25873cf13ae74
yz19a <yanelperaltarodriguez77@gmail.com>
2025-03-23 15:59:58 +0100

    made functions

---
+++ sources/check_args.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_args.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaperalt <yaperalt@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/19 15:19:08 by yaperalt          #+#    #+#             */
+/*   Updated: 2025/03/19 15:19:32 by yaperalt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+
+void	*process_token(t_stack **stack, char *token)
+{
+	int	num;
+
+	int	error;
+
+	error = 0;
+	if (!(is_valid_number(token)))
+	{
+		write(2, "arg error\n", 10);
+		free_list(*stack);
+		exit(EXIT_FAILURE);
+	}
+	num = ft_atoi(token, &error);
+	if (error || has_duplicate(*stack, num))
+	{
+		write(2, "arg error2\n", 10);
+		free_list(*stack);
+		exit(EXIT_FAILURE);
+	}
+	add_node(stack, num);
+}
+
+void	parse_args(t_stack **stack, int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	*token;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			while (argv[i][j] && ft_isspace(argv[i][j]))
+				j++;
+			if (!argv[i][j])
+				break;
+			token = &argv[i][j];
+			while (argv[i][j] && !ft_isspace(argv[i][j]))
+				j++;
+			argv[i][j] = '\0';
+			process_token(stack, token);
+		}
+		i++;
+	}
+}
