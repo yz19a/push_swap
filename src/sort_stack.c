8a7e328fb7de80f3604b4b3c1df343992c492eb5
yanel <yanelperaltarodriguez77@gmail.com>
2025-04-12 17:35:05 +0200

    changed line 27

--- src/sort_stack.c
+++ src/sort_stack.c
@@ -6,28 +6,25 @@
 /*   By: yaperalt <yaperalt@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 17:14:02 by yaperalt          #+#    #+#             */
-/*   Updated: 2025/04/10 01:39:36 by yaperalt         ###   ########.fr       */
+/*   Updated: 2025/04/12 17:34:06 by yaperalt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 #include <stdio.h>
 
-void	sort_stack(t_stack **a, t_stack **b)
+void sort_stack(t_stack **a, t_stack **b)
 {
-	int	size_stack;
+	int size_stack;
 
 	if (!a || !(*a))
-		return ;
+		return;
 	if (is_sorted(*a))
-		return ;
+		return;
 	prepare_stack(a);
 	size_stack = stack_size(*a);
 	if (size_stack == 2)
-	{
-		if ((*a)->fix_v > (*a)->next->fix_v)
-			sa(a, 1);
-	}
+		sort_two(a);
 	else if (size_stack == 3)
 		sort_three(a);
 	else if (size_stack == 4 || size_stack == 5)
